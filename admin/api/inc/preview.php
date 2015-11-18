14038320432acffed13ab1a23181e84cf89aefe8
Moncho Varela <nakome@gmail.com>
2015-11-18 13:01:15 +0100

    Upload dev

--- admin/api/inc/preview.php
+++ admin/api/inc/preview.php
@@ -11,15 +11,15 @@
 * @name   Download files
 */
 $p->route(array('/action/backups/download/(:any)/(:any)'), function($token,$file) use($p){
-	if(Session::exists('user')){
-		if (Token::check($token)) {
-			$path = base64_decode($file);
-			$download = str_replace(ROOTBASE,'',$path);
-			Request::redirect($p::$site['url'].$download);
-		}else{
-			die('crsf detect!');
-		}
-	}
+  if(Session::exists('user')){
+    if (Token::check($token)) {
+      $path = base64_decode($file);
+      $download = str_replace(ROOTBASE,'',$path);
+      Request::redirect($p::$site['url'].$download);
+    }else{
+      die('crsf detect!');
+    }
+  }
 });
 
 
@@ -37,11 +37,11 @@ $p->route(array('/action/backups/download/(:any)/(:any)'), function($token,$file
 * @desc   Open preview on blank page
 */
 $p->route(array('/action/preview/(:any)'), function($file) use($p){
-		// remove dir
-		$link = str_replace(PAGES, '', base64_decode($file));
-		// remove .md
-		$link = str_replace('.md', '', $link);
-		Request::redirect($p::$site['url'].$link);
+    // remove dir
+    $link = str_replace(PAGES, '', base64_decode($file));
+    // remove .md
+    $link = str_replace('.md', '', $link);
+    Request::redirect($p::$site['url'].$link);
 });
 
 
@@ -58,62 +58,62 @@ $p->route(array('/action/preview/(:any)'), function($file) use($p){
 * @desc   Open preview on blank page
 */
 $p->route('/action/uploads/preview/(:any)', function($file) use($p){
-		// remove dir
-		$link = str_replace(UPLOADS, '', base64_decode($file));
-		$link = str_replace('\\', '/', $link);
-		$link = str_replace('//', '/', $link);
-		// check mime types
-		$template = '';
-		// decode file
-		$path = base64_decode($file);
-		$link = Url::sanitizeURL($link);
-		// check mime types
-		if(File::mime($path)){
-			if(File::ext($path) == 'jpg'  || File::ext($path) == 'JPG'  ||
-				File::ext($path) == 'png'   || File::ext($path) == 'PNG'  ||
-				File::ext($path) == 'jpeg'  || File::ext($path) == 'JPEG' ||
-				File::ext($path) == 'gif'){
-				// get image size
-				list($width, $height) = getimagesize($path);
-				// image template
-				$template = '
-					<div class="col-lg-6">
-						<img class="img-responsive thumbnsil" src="'.$p::$site['url'].'/public/uploads/'.$link.'"/>
-					</div>
-					<div class="col-lg-6">
-						<ul class="list-group">
-							<li class="list-group-item"><b>Filename: </b>'.File::name($path).'</li>
-							<li class="list-group-item"><b>Extension: </b>'.File::ext($path).'</li>
-							<li class="list-group-item"><b>Size: </b>'.$width.'x'.$height.'px</li>
-							<li class="list-group-item"><b>Markdown: </b><code>![text img](<a target="_blank" href="'.Panel::$site['url'].'/public/uploads/'.$link.'">'.Panel::$site['url'].'/public/uploads/'.$link.'</a>){.img-responsive}</code></li>
-							<li class="list-group-item"><b>Html: </b><code>&lt;img src="<a target="_blank" href="'.Panel::$site['url'].'/public/uploads/'.$link.'">'.Panel::$site['url'].'/public/uploads/'.$link.'</a> class="img-responsive" /&gt;</code></li>
-							<li class="list-group-item"><a class="btn btn-danger" href="'.$p->url().'/uploads">'.Panel::$lang['back_to_uploads'].'</a></li>
-						</ul>
-					</div>';
+    // remove dir
+    $link = str_replace(UPLOADS, '', base64_decode($file));
+    $link = str_replace('\\', '/', $link);
+    $link = str_replace('//', '/', $link);
+    // check mime types
+    $template = '';
+    // decode file
+    $path = base64_decode($file);
+    $link = Url::sanitizeURL($link);
+    // check mime types
+    if(File::mime($path)){
+      if(File::ext($path) == 'jpg'  || File::ext($path) == 'JPG'  ||
+        File::ext($path) == 'png'   || File::ext($path) == 'PNG'  ||
+        File::ext($path) == 'jpeg'  || File::ext($path) == 'JPEG' ||
+        File::ext($path) == 'gif'){
+        // get image size
+        list($width, $height) = getimagesize($path);
+        // image template
+        $template = '
+          <div class="col-lg-6">
+            <img class="img-responsive thumbnsil" src="'.$p::$site['url'].'/public/uploads/'.$link.'"/>
+          </div>
+          <div class="col-lg-6">
+                <ul class="list-group">
+                  <li class="list-group-item"><b>Filename: </b>'.File::name($path).'</li>
+                  <li class="list-group-item"><b>Extension: </b>'.File::ext($path).'</li>
+                  <li class="list-group-item"><b>Size: </b>'.$width.'x'.$height.'px</li>
+                  <li class="list-group-item"><b>Markdown: </b><code>![text img](<a target="_blank" href="'.Panel::$site['url'].'/public/uploads/'.$link.'">'.Panel::$site['url'].'/public/uploads/'.$link.'</a>){.img-responsive}</code></li>
+                  <li class="list-group-item"><b>Html: </b><code>&lt;img src="<a target="_blank" href="'.Panel::$site['url'].'/public/uploads/'.$link.'">'.Panel::$site['url'].'/public/uploads/'.$link.'</a> class="img-responsive" /&gt;</code></li>
+                  <li class="list-group-item"><a class="btn btn-danger" href="'.$p->url().'/uploads">'.Panel::$lang['back_to_uploads'].'</a></li>
+                </ul>
+          </div>';
 
-			}else{
-				// other template files
-				$template = '
-				<div class="col-lg-6">
-					<ul class="list-group">
-						<li class="list-group-item">'.Panel::$lang['no_preview_for_this_file'].'</li>
-						<li class="list-group-item"><b>Filename: </b>'.File::name($path).'</li>
-						<li class="list-group-item"><b>Extension: </b>'.File::ext($path).'</li>
-						<li class="list-group-item"><b>Markdown: </b><code>[text link](<a target="_blank" href="'.Panel::$site['url'].'/public/uploads/'.$link.'">'.Panel::$site['url'].'/public/uploads/'.$link.'</a>)</code></li>
-						<li class="list-group-item"><b>Html: </b><code>&lt;a href="<a target="_blank" href="'.Panel::$site['url'].'/public/uploads/'.$link.'">'.Panel::$site['url'].'/public/uploads/'.$link.'</a>" download &gt;text link&lt;/a&gt;</code></li>
-						<li class="list-group-item"><a class="btn btn-danger" href="'.$p->url().'/uploads">'.Panel::$lang['back_to_uploads'].'</a></li>
-					</ul>
-				</div>';
+      }else{
+        // other template files
+        $template = '
+        <div class="col-lg-6">
+              <ul class="list-group">
+                <li class="list-group-item">'.Panel::$lang['no_preview_for_this_file'].'</li>
+                <li class="list-group-item"><b>Filename: </b>'.File::name($path).'</li>
+                <li class="list-group-item"><b>Extension: </b>'.File::ext($path).'</li>
+                <li class="list-group-item"><b>Markdown: </b><code>[text link](<a target="_blank" href="'.Panel::$site['url'].'/public/uploads/'.$link.'">'.Panel::$site['url'].'/public/uploads/'.$link.'</a>)</code></li>
+                <li class="list-group-item"><b>Html: </b><code>&lt;a href="<a target="_blank" href="'.Panel::$site['url'].'/public/uploads/'.$link.'">'.Panel::$site['url'].'/public/uploads/'.$link.'</a>" download &gt;text link&lt;/a&gt;</code></li>
+                <li class="list-group-item"><a class="btn btn-danger" href="'.$p->url().'/uploads">'.Panel::$lang['back_to_uploads'].'</a></li>
+              </ul>
+          </div>';
 
-			}
-		}
+      }
+    }
 
-		$p->view('actions',array(
-			'type' => 'Upload Preview',
-			'title' => Panel::$lang['Preview'],
-			'content' => $file,
-			'html' => $template
-		));
+    $p->view('actions',array(
+      'type' => 'Upload Preview',
+      'title' => Panel::$lang['Preview'],
+      'content' => $file,
+      'html' => $template
+    ));
 });
 
 
--- admin/api/inc/tools.php
+++ admin/api/inc/tools.php
@@ -9,20 +9,20 @@
 * @desc   Clear cache on click 
 */
 $p->route('/action/clearCache/(:any)/', function($token) use($p){
-	if(Session::exists('user')){
-		if (Token::check($token)) {
-			$cache = File::scan(CACHE.'/fenom','php');
-			foreach ($cache as $item) {
-				File::delete($item);
-			}
-			// set notification
-			$p->setMsg($p::$lang['Success_cache']);
-			// redirect
-			Request::redirect($p->Url().'/pages');
-		}else{
-			die('Crsf detect !');
-		}
-	}else{
-		Request::redirect($p::$site['url'].'/'.$p::$site['backend_folder']);
-	}
+  if(Session::exists('user')){
+    if (Token::check($token)) {
+      $cache = File::scan(CACHE.'/fenom','php');
+      foreach ($cache as $item) {
+        File::delete($item);
+      }
+      // set notification
+      $p->setMsg($p::$lang['Success_cache']);
+      // redirect
+      Request::redirect($p->Url().'/pages');
+    }else{
+      die('Crsf detect !');
+    }
+  }else{
+    Request::redirect($p::$site['url'].'/'.$p::$site['backend_folder']);
+  }
 });
